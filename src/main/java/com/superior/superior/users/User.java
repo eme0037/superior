6dcf3a59280b7b3580ce8559ab8f338ebfacc109
Elise <forsale@Elises-MacBook-Pro.local>
2025-12-29 15:06:46 -0600

    Add constructer and getters/setters to user

--- src/main/java/com/superior/superior/users/User.java
+++ src/main/java/com/superior/superior/users/User.java
@@ -6,4 +6,52 @@ public class User {
     private String email;
     private String password;
     private Role role;
+
+    public User(Long id, String email, String password, Role role) { // constructor
+
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    // getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+
+
+
+
+
 }
